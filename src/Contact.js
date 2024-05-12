85653c31e46eb341da67dd542ea307c72701a3d6
Vaibhav Tiwari <tiwarivaibhav1410@gmail.com>
2024-05-12 14:12:03 +0530

    added contact component

--- src/Contact.js
+++ src/Contact.js
@@ -33,7 +33,57 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact page</h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3697.4223748970485!2d82.1473194747429!3d22.071680851144297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a280b2279efce69%3A0xafca15c9fef60c06!2sRama%20Magneto%20Mall%2C%20Bilaspur!5e0!3m2!1sen!2sin!4v1715501344033!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: "0" }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/mrgndqej"
+            method="post"
+            className="contact-inputs"
+          >
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="Email"
+              required
+              autoComplete="off"
+            />
+
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter your Message"
+            ></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
